9aa2f98b98e6661f08a2d51f39ad909e6c88e7eb
Sai Kiran Dasika <kirandasika30@gmail.com>
2017-10-23 09:44:36 -0500

    corrected typo in signup logic

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def sign_up():
 		#returning true or returning to signup page
 		attempted_username = request.form['username']
 		attempted_password = request.form['password']
-		if len(attempted_username) > 1 or len(attempted_password) < 1:
+		if len(attempted_username) < 1 or len(attempted_password) < 1:
 			return jsonify({"response": False, "error": "please provide a username/password"})
 
 		pass_hash = bcrypt.generate_password_hash(attempted_password)
