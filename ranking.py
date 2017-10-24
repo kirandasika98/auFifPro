4183736b3f2e00a7a89923c8462f32392e3efaf8
Sai Kiran Dasika <kirandasika30@gmail.com>
2017-10-23 21:03:00 -0500

    added matches played in dashboard and updated ranking alg with mp.

--- ranking.py
+++ ranking.py
@@ -14,23 +14,28 @@ class RankUser():
 		self.draws = 0
 		self.GD = 0
 		self.points = 0
+		self.matches_played = 0
 
 	def add_win(self):
 		self.wins += 1
 		self.points += 3
 
-	
+
 	def add_draw(self):
 		self.draws += 1
 		self.points += 1
 
-	
+
 	def calculate_GD(self, GF, GA):
-		self.GD += (GF - GA) 
+		self.GD += (GF - GA)
+
+	def add_match(self):
+		self.matches_played += 1
 
 	def __str__(self):
-		return "id:{}, username:{}, wins:{}, draws:{}, GD:{}, points:{}".format(self.id,
-				self.username, self.wins, self.draws, self.GD, self.points)
+		return "id:{}, username:{}, wins:{}, draws:{}, GD:{}, points:{}, mp:{}".format(self.id,
+				self.username, self.wins, self.draws, self.GD, self.points,
+				self.matches_played)
 
 
 def calculate_ranks():
@@ -47,7 +52,7 @@ def calculate_ranks():
 			player2 = user_map[match.player2.get_id()]
 		except KeyError:
 			player2 = RankUser(match.player2.get_id(), match.player2.username)
-		
+
 		# calculating draws and wins
 		if match.player1_goals == match.player2_goals:
 			# add a point for both players
@@ -62,9 +67,13 @@ def calculate_ranks():
 		player1.calculate_GD(match.player1_goals, match.player2_goals)
 		player2.calculate_GD(match.player2_goals, match.player1_goals)
 
+		# adding a match for both players
+		player1.add_match()
+		player2.add_match()
+
 		# adding users back to the player user_map
 		user_map[player1.id] = player1
 		user_map[player2.id] = player2
 
-	return OrderedDict(sorted(user_map.items(), key=lambda t: t[1].points, reverse=True))
-
+	return OrderedDict(sorted(user_map.items(), key=lambda t: (t[1].points, t[1].matches_played),
+ 							reverse=True))
